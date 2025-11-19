9783918659ebd86c146dd3673f35e8a33215a594
Litded <67542000+litded@users.noreply.github.com>
2025-11-19 11:29:54 +0300

    Update vmname.sh

--- vmname.sh
+++ vmname.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VMNAME=$(sed -n 's/.*VirtualMachineName\x00\(.*\)\x00.*/\1/p' /var/lib/hyperv/.kvp_pool_3' -e 's/\x0.*//g')
+VMNAME=$(cat /var/lib/hyperv/.kvp_pool_3 | sed -e 's/.*VirtualMachineName\x00\(.*\)\x00.*/\1/p' -e 's/\x0.*//g')
 hostname $VMNAME
 sed -iE "s/ localhost.*/ localhost $VMNAME/g" /etc/hosts
